adab4d719cbf35e69b4418202b1fdf1e9f4975d0
Aleksey Kulgeyko <kulgeyko93@mail.ru>
2020-10-19 17:54:30 +0300

    docs: complite

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) return [];
+  if ( matrix.length === 1 && matrix[0] === undefined) return matrix;
+  let result = [];
+  matrix.map( (item, index) => {
+    if ( index % 2 !== 0){
+      return result.push(...item.reverse()) 
+    }
+
+     return result.push(...item) 
+  })
+
+  return result;
 }
